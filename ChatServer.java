b34273d29f4b94bdfacccd38ad9960ff83d29d7b
Cheila Alves <up201805089@fc.up.pt>
2020-11-20 17:36:08 +0000

    Código desenvolvido na ficha 5

--- ChatServer.java
+++ ChatServer.java
@@ -4,9 +4,10 @@ import java.nio.*;
 import java.nio.channels.*;
 import java.nio.charset.*;
 import java.util.*;
+import java.io.IOException;
 
 public class ChatServer {
-
+    
     // buffer pré-alocado para os dados recebidos
     static private final ByteBuffer buffer =
 	ByteBuffer.allocate(16384);
@@ -53,19 +54,21 @@ public class ChatServer {
 		// e processar cada uma
 		Set<SelectionKey> keys = selector.selectedKeys();
 		Iterator<SelectionKey> it = keys.iterator();
+		boolean canReadName = false;
 		while (it.hasNext()) {
 		    // obter a chave que representa um dos bits da atividade I/O
 		    SelectionKey key = it.next();
 
 		    // que tipo de atividade é?
 		    if (key.isAcceptable()) {
+			
 			// é uma conexão futura. Registar a socket com
 			// o Selector para que possamos ouvir input nela
 			Socket s = ss.accept();
 			System.out.println("Got connection from " + s);
 
 			// assegurar que seja non-blocking, para que possamos usar um selector nela
-			SocketChannel sc = s.getChannel();
+			SocketChannel sc = s.getChannel();		
 			sc.configureBlocking(false);
 
 			// registá-la com o selector, para leitura
@@ -73,28 +76,57 @@ public class ChatServer {
 			
 		    } else if (key.isReadable()) {
 
+			// --------------------------------------------------------------------------------
+			
 			SocketChannel sc = null;
 
 			try {
 
 			    // dados futuros numa conexão -- processá-los
 			    sc = (SocketChannel)key.channel();
-			    boolean ok = processInput(sc);
 
-			    // se a conexão está morta, removê-la do selector e fechá-la
-			    if (!ok) {
+			    // gera uma sequência mutável de caracteres, útil quando há várias threads
+			    StringBuilder sb = new StringBuilder(); 
+
+			    buffer.clear();
+			    
+			    int read = 0;
+			    while ((read = sc.read(buffer)) > 0) { // enquanto não for atingido o fim da stream
 				
-				key.cancel();
-
-				Socket s = null;
-				try {
-				    s = sc.socket();
-				    System.out.println("Closing connection to " + s);
-				    s.close();
-				} catch (IOException ie) {
-				    System.err.println("Error closing socket " + s + ": " + ie);
-				}
+				buffer.flip(); // preparar o buffer
+				byte[] bytes = new byte[buffer.limit()];
+				buffer.get(bytes);
+				sb.append(new String(bytes));
+				buffer.clear();
+				
 			    }
+			    
+			    String msg;
+			    if(read < 0) { // se estivermos no fim da stream a conexão é fechada
+				
+				msg = key.attachment() + " left the chat.\n";
+				sc.close();
+				
+			    } else { // caso contrário, a msg prossegue
+				
+				msg = key.attachment() + ": " + sb.toString();
+				
+			    }
+
+			    // difusão da mensagem a todos os clientes ativos
+			    ByteBuffer msgBuf = ByteBuffer.wrap(msg.getBytes());
+			    for(SelectionKey k : selector.keys()) {
+
+				if(k.isValid() && k.channel() instanceof SocketChannel && k != key) {
+				    SocketChannel sch = (SocketChannel)k.channel();
+				    sch.write(msgBuf);
+				    msgBuf.rewind();
+				}
+				
+			    }
+			    
+			    // --------------------------------------------------------------------------------
+			    
 			} catch (IOException ie) {
 			    // em exceção, remove-se este canal do selector
 			    key.cancel();
@@ -111,29 +143,11 @@ public class ChatServer {
 
 		// removemos as chaves selecionadas, porque já lidámos com elas
 		keys.clear();
+		
 	    }
+	    
 	} catch (IOException ie) {
 	    System.err.println(ie);
 	}
-    }
-
-    // apenas ler a mensagem da socket e enviá-la para o stdout
-    static private boolean processInput(SocketChannel sc) throws IOException {
-
-	// ler a mensagem do buffer
-	buffer.clear();
-	sc.read(buffer);
-	buffer.flip();
-
-	// se não há dados, fechar conexão
-	if (buffer.limit() == 0) {
-	    return false;
-	}
-
-	// descodificar e imprimir a mensagem para o stdout
-	String message = decoder.decode(buffer).toString();
-	System.out.println(message);
-
-	return true;
     }
 }
